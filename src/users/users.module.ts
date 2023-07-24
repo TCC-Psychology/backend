aeb5d8459640281768685314036e0ea3537d51b0
lucaslopesx <lucas.lopesx1@gmail.com>
2023-07-23 22:22:19 -0300

    remove Firebase from project

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'prisma/prisma.service';
-import { FirebaseModule } from 'src/firebase/firebase.module';
 
 @Module({
-  imports: [FirebaseModule],
+  imports: [],
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
 })
